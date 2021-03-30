bc7bd127c0333998327e3892057419b4a4a06aad
basil <basil@sprintive.com>
2021-03-30 15:41:33 +0300

    saif

--- app.js
+++ app.js
@@ -34,32 +34,4 @@ $(document).ready(() => {
     const x = JSON.stringify(itemsToDelete());
     $.post("http://localhost/filemanager.php", { itemsToDelete: x });
   });
-
-  $(".fa-trash-alt").click(function () {
-    const x = JSON.stringify(itemsToDelete());
-    $.ajax({
-      url: "filemanager.php",
-      type: "POST",
-      ContentType: "application/json",
-      data: { itemsToDelete: x },
-    })
-      .done(function (response) {
-        alert("success");
-      })
-      .fail(function (jqXHR, textStatus, errorThrown) {
-        alert("FAILED! ERROR: " + errorThrown);
-      });
-    // $.ajax({
-    //   type: "POST",
-    //   url: "filemanager.php",
-    //   data: { itemsToDelete: x },
-    //   contentType: "application/json",
-    //   success: function (msg) {
-    //     alert(msg);
-    //   },
-    //   error: function (err) {
-    //     alert(err.responseText);
-    //   },
-    // });
-  });
 });
